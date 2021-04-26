06f2dacd97c028d87d6655c30d0f1813ec13694b
OyugoObonyo <nyikal1999@gmail.com>
2021-04-26 17:31:00 +0300

    Manipulating lists

---
+++ LISTS/cities.py
@@ -0,0 +1,20 @@
+cities = ["Nairobi", "Harare", "Kampala", "Lagos", "Dodoma"]
+
+# **********PRINT LIST IN ORIGINAL ORDER**********
+print(cities)
+
+# ***********TEMPORARILY SORT LIST***********
+sorted(cities)
+print(cities)
+
+# ***********REVERSE LIST ORDER***********
+print(cities)
+cities.reverse()
+print(cities)
+
+# ***********PERMANENTLY SORT LIST*********
+cities.sort()
+print(cities)
+
+# **********PRINT STRING LENGTH**********
+print(len(cities))
