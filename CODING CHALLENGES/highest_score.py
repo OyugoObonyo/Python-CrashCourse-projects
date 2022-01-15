4ed7c7a8e776daab05517a57302f35c0dce79dd0
OyugoObonyo <nyikal1999@gmail.com>
2022-01-15 11:36:38 +0300

    Find the highest score from a list of scores

---
+++ CODING CHALLENGES/highest_score.py
@@ -0,0 +1,15 @@
+"""
+Python module that takes a list as arg and
+returns the highest value in the list without 
+using the max or min function
+"""
+def highest_score(scores):
+    max_score = 0
+    for score in scores:
+        if score > max_score:
+            max_score = score
+    print(f"Highest value in the list is: {max_score}")
+    return max_score
+
+if __name__ == "__main__":
+    highest_score([78,65,89,86,55,91,64,89])
