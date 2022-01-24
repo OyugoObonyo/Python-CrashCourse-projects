1b1f6010929634cd7b15f8d0241a3916a7854515
OyugoObonyo <nyikal1999@gmail.com>
2022-01-24 19:56:27 +0300

    Move assesment file

--- SQL/GROUP_BY STATEMENTS/tennis_slot.sql
+++
@@ -1,8 +0,0 @@
--- Get tennis courts available on 21/9/2012
-
-SELECT starttime, cd.facilities.name FROM cd.bookings
-JOIN cd.facilities
-ON cd.bookings.facid = cd.facilities.facid 
-WHERE cd.facilities.name IN ('Tennis Court 1', 'Tennis Court 2')
-AND starttime >= '2012-09-21' 
-AND starttime <= '2012-09-22'
