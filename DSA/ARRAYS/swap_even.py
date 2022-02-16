74b59b64ed713237e17f54bcbaa7010674dc085c
OyugoObonyo <nyikal1999@gmail.com>
2022-02-16 10:19:07 +0300

    update swap_even file

--- DSA/ARRAYS/swap_even.py
+++ DSA/ARRAYS/swap_even.py
@@ -1,9 +1,10 @@
 """
 Module with a func that takes an array and waps even numbers to be at the beginning of array
 """
-from traceback import print_tb
 import unittest
 
+'''
+My algo
 def swap_even(array):
     """
     takes array and sorts it such that even numbers appear first
@@ -23,6 +24,7 @@ def swap_even(array):
 
 result = swap_even([1,2,4,5,8,6])
 print(result)
+'''
 
 """
 class TestCase(unittest.TestCase):
@@ -34,3 +36,20 @@ class TestCase(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 """
+
+'''
+SUMMARY: Algo shortcomings
+Algo falls apart in the event of 2 successive odd numbers
+In python, swapping can be done through assignment of the same line i.e a,b = b,a 
+solution :
+Start looping through and filling array values from the back,
+compare first and last array value till you're back at the first value
+def even_odd (A) :
+    next_even, next-odd = 0, len(A) - I
+    while next-even < next_odd:
+        if A[next-even] % 2 == 0:
+            next_even += 1
+        else:
+            A[next-even], A[next_odd] = A[next_odd], A[next_even]
+            next_odd -= 1
+'''
